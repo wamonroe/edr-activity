d58bf73f1c099f5f7c866a8f5ddd5322ac43fc94
Alex Monroe <alex@monroepost.com>
2023-05-08 09:14:42 -0400

    Use Process#spawn on windows

--- spec/lib/edr_activity/framework/network/tcp_spec.rb
+++ spec/lib/edr_activity/framework/network/tcp_spec.rb
@@ -10,17 +10,28 @@ RSpec.describe EDRActivity::Framework::Network do
   around do |ex|
     @server = TCPServer.new("127.0.0.1", 0)
     @port = @server.addr[1]
-    fork do
-      loop do
-        client = @server.accept
-        client.print "hello there"
-        client.close
-        break
+    if Gem.win_platform?
+      # Use Process#spawn on windows
+      @server.close # now that we know an open port, close it and reuse it below
+      @pid = Process.spawn(
+        "ruby -e \"require 'socket'; s = TCPServer.new('127.0.0.1', #{@port}); " \
+        "loop { c = s.accept; c.print 'hello there'; c.close; break; }\""
+      )
+    else
+      # Use Process#fork on linux and mac
+      @pid = Process.fork do
+        loop do
+          client = @server.accept
+          client.print "hello there"
+          client.close
+          break
+        end
       end
     end
     ex.run
   ensure
     @server&.close
+    Process.kill(:TERM, @pid) if @pid
   end
 
   it "sends the message to the destination host and returns the response" do
